4a284bb5085f1df19bce4f3d21fa60f6ab22d76b
Amirhossein <Amirmemool12@gmail.com>
2023-08-04 19:22:00 +0330

    :heavy_plus_sign: feat: create invoices table migration

---
+++ database/migrations/2023_08_04_154411_create_invoices_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::create('invoices', function (Blueprint $table) {
+            $table->id();
+            $table->integer('customer_id');
+            $table->integer('amount');
+            $table->string('status');
+            $table->dateTime('billed_date');
+            $table->dateTime('paid_date')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('invoices');
+    }
+};
