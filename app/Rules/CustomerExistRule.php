3545ee23ffb0f92f2da4394616bca9abfb2677be
Amirhossein <Amirmemool12@gmail.com>
2023-08-04 21:22:45 +0330

    :heavy_plus_sign: feat: create CustomerExist Rule

---
+++ app/Rules/CustomerExistRule.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Rules;
+
+use App\Models\Customer;
+use Illuminate\Contracts\Validation\Rule;
+
+class CustomerExistRule implements Rule
+{
+    /**
+     * Create a new rule instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+
+    }
+
+    /**
+     * Determine if the validation rule passes.
+     *
+     * @param  string  $attribute
+     * @param  mixed  $value
+     * @return bool
+     */
+    public function passes($attribute, $value): bool
+    {
+        return Customer::where('id',$value)->exists();
+    }
+
+    /**
+     * Get the validation error message.
+     *
+     * @return string
+     */
+    public function message(): string
+    {
+        return 'The customer is not Exist';
+    }
+}
