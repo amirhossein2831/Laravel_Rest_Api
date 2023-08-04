3e97a1c49d10030711b4d2d434a3186b541508e1
Amirhossein <Amirmemool12@gmail.com>
2023-08-04 19:30:34 +0330

    :heavy_plus_sign: feat: implement random value for the customer in Factory class

---
+++ database/factories/CustomerFactory.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Customer;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Customer>
+ */
+class CustomerFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $type = $this->faker->randomElement(['P', 'B']);                                //P -->person      B -->business
+        $name = $type === 'P' ? $this->faker->name():$this->faker->company();
+
+        return [
+            'name' => $name,
+            'type' => $type,
+            'email' => $this->faker->email(),
+            'address'=> $this->faker->streetAddress(),
+            'city' => $this->faker->city(),
+            'stat'=>$this->faker->state(),
+            'postal_code'=> $this->faker->postcode()
+        ];
+    }
+}
