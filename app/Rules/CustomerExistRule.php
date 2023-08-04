8a31c985100972cdd846ae3a1c511839081bd3cb
Amirhossein <Amirmemool12@gmail.com>
2023-08-04 21:25:46 +0330

    :recycle: fix: delete unusefull code

--- app/Rules/CustomerExistRule.php
+++ app/Rules/CustomerExistRule.php
@@ -8,25 +8,15 @@ use Illuminate\Contracts\Validation\Rule;
 class CustomerExistRule implements Rule
 {
     /**
-     * Create a new rule instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-
-    }
-
-    /**
      * Determine if the validation rule passes.
      *
-     * @param  string  $attribute
-     * @param  mixed  $value
+     * @param string $attribute
+     * @param mixed $value
      * @return bool
      */
     public function passes($attribute, $value): bool
     {
-        return Customer::where('id',$value)->exists();
+        return Customer::where('id', $value)->exists();
     }
 
     /**
