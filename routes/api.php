9a8c6d00c52a32f349b53c9da37c0b2f4df96cd2
Amirhossein <Amirmemool12@gmail.com>
2023-08-04 19:47:23 +0330

    :hammer: chore: implement the routes:

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\CustomerController;
+use App\Http\Controllers\InvoiceController;
+use App\Models\Invoice;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +19,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+Route::group(['prefix' => 'v1'], function () {
+    Route::apiResource('customer', CustomerController::class);
+    Route::apiResource('invoice', InvoiceController::class);
 });
