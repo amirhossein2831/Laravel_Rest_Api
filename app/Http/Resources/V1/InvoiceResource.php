430c78043734e12036c28cead02f98946f8e05a1
Amirhossein <Amirmemool12@gmail.com>
2023-08-04 20:03:14 +0330

    :heavy_plus_sign: feat: create Invoice resourse

---
+++ app/Http/Resources/V1/InvoiceResource.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Resources\V1;
+
+use Illuminate\Contracts\Support\Arrayable;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+use JsonSerializable;
+
+class InvoiceResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  Request  $request
+     * @return array|Arrayable|JsonSerializable
+     */
+    public function toArray($request): array|JsonSerializable|Arrayable
+    {
+        return [
+            'customerId'=> $this->customer_id,
+            'amount'=>$this->amount,
+            'status'=>$this->status,
+            'billedDate'=>$this->billed_date,
+            'paidDate'=>$this->paid_date,
+        ];
+    }
+}
