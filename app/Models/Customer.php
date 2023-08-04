86c53b137477ba355eefe39d6364497439420478
Amirhossein <Amirmemool12@gmail.com>
2023-08-04 19:17:32 +0330

    :heavy_plus_sign: feat: create: Customer and Invoices Models and the ralation ships

---
+++ app/Models/Customer.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Customer extends Model
+{
+    use HasFactory;
+
+    public function invoices(): HasMany
+    {
+        return $this->hasMany(Invoice::class);
+    }
+}
