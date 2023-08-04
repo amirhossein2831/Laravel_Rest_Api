cfd80a6aee30fab16fc8418132e9c2174c03af80
Amirhossein <Amirmemool12@gmail.com>
2023-08-04 21:35:10 +0330

    :hammer: chore: add invoices to the customers

--- app/Http/Resources/V1/CustomerResource.php
+++ app/Http/Resources/V1/CustomerResource.php
@@ -24,7 +24,8 @@ class CustomerResource extends JsonResource
             'address'=> $this->address,
             'city'=> $this->city,
             'state' => $this->state,
-            'postalCode'=> $this->postal_code
+            'postalCode'=> $this->postal_code,
+            'invoices'=> InvoiceResource::collection($this->whenLoaded('invoices'))
         ];
     }
 }
