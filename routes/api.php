f630510fe3e031591fa3e540a0e780171a596f88
Amirhossein <Amirmemool12@gmail.com>
2023-08-04 23:26:45 +0330

    :hammer: chore: add auth Middle ware to the api routes

--- routes/api.php
+++ routes/api.php
@@ -19,7 +19,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::group(['prefix' => 'v1'], function () {
+Route::group(['prefix' => 'v1','middleware' => 'auth:sanctum'], function () {
     Route::apiResource('customer', CustomerController::class);
     Route::apiResource('invoice', InvoiceController::class);
     Route::post('invoice/bulk',[InvoiceController::class,'bulkStore']);
