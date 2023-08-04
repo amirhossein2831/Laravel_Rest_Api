89b2693414ca1cf0b9138127b68ddee909f9e682
Amirhossein <Amirmemool12@gmail.com>
2023-08-04 22:00:08 +0330

    :heavy_plus_sign: feat: create UpdateCustomerRequest Class and define the rule

---
+++ app/Http/Requests/UpdateCustomerRequest.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class UpdateCustomerRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules(): array
+    {
+        if ($this->isMethod('PUT')) {
+            $rules = [
+                'name' => 'required',
+                'type' => ['required', Rule::in(["P", "p", "B", 'b'])],
+                'email' => 'required|email',
+                'address' => 'required',
+                'city' => 'required',
+                'state' => 'required',
+                'postalCode' => 'required',
+            ];
+        } else
+            $rules = [
+                'name' => 'sometimes|required',
+                'type' => ['sometimes', 'required', Rule::in(["P", "p", "B", 'b'])],
+                'email' => 'sometimes|required|email',
+                'address' => 'sometimes|required',
+                'city' => 'sometimes|required',
+                'state' => 'sometimes|required',
+                'postalCode' => 'sometimes|required',
+            ];
+        return $rules;
+    }
+
+    protected function prepareForValidation(): void
+    {
+        if ($this->postalCode) {
+            $this->merge(
+                [
+                    'postal_code' => $this->postalCode
+                ]
+            );
+        }
+    }
+}
