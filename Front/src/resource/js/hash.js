1dd1245d0a89ca91a3a862ed72317ad46ea1ee40
Amirhossein <Amirmemool12@gmail.com>
2023-09-15 01:56:38 +0330

    :hammer: chore: add invoice to customer Page

--- Front/src/resource/js/hash.js
+++ Front/src/resource/js/hash.js
@@ -1,4 +1,6 @@
 import {userInfo} from './component.js';
+import {invoice} from './component.js';
+
 
 window.addEventListener('hashchange', loadContentBasedOnHash);
 
@@ -11,7 +13,16 @@ function loadContentBasedOnHash() {
                  ${userInfo()}
                </div>
             </div>
-
+            <div class="container">
+                <div class="row border border-4 border-info rounded-3 mt-3" style="font-size: 25px">
+                    <h1 class="mt-2">Customer Invoices</h1>
+                    <div class="accordion" id="accordionExample">
+                    ${invoice()}
+                    ${invoice()}
+                    ${invoice()}
+                    </div>
+                </div>
+            </div>
             `;
         }
         else if (hash === '#2') {
