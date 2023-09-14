cc1e1c0778019071b3aee56d6a71f463b3b2160a
Amirhossein <Amirmemool12@gmail.com>
2023-09-15 01:47:12 +0330

    :hammer: chore: add userinfo to the userPage

--- Front/src/resource/js/hash.js
+++ Front/src/resource/js/hash.js
@@ -1,9 +1,18 @@
+import {userInfo} from './component.js';
+
 window.addEventListener('hashchange', loadContentBasedOnHash);
 
 function loadContentBasedOnHash() {
     const hash = window.location.hash;
         if (hash === '#1') {
-            document.getElementById('body').innerHTML= '';
+            document.getElementById('body').innerHTML= `
+            <div class="container">
+                 <div class="row border border-4 border-info rounded-3 mt-5" style="font-size: 25px">
+                 ${userInfo()}
+               </div>
+            </div>
+
+            `;
         }
         else if (hash === '#2') {
             console.log(2);
